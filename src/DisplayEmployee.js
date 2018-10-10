a9b2251ab6136207108f2fa9fc7f86f7c356afab
Bruno Belorgey <bruno.belorgey@laposte.net>
2018-10-10 09:32:36 +0200

    fetch simpson avec un petit css

--- src/DisplayEmployee.js
+++ src/DisplayEmployee.js
@@ -1,12 +1,12 @@
 import  React  from  'react';
+import "./style.css";
 
 const  DisplayEmployee = ({ employee }) => {
     return (
         <div  className="DisplayEmployee">
             <img  src={employee.image}  alt="picture"  />
-            <div>{employee.quote}</div>
+            <quote DisplayCharacter>{employee.quote}</quote>
             <div>{employee.character}</div>
-            <div>{employee.characterDirection}</div>
         </div>
     );
 };
